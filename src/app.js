dfad694a9d26e07cc4c87661d55555032319f84a
Alan <159034743+AlanLeandroVargas@users.noreply.github.com>
2024-10-06 09:10:17 -0300

    Autenticacion implementada

--- src/app.js
+++ src/app.js
@@ -2,20 +2,22 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { retrieveGamesByGenre } from './index.js';
+import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken'
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, '..', 'public', 'views'));
 
 
-app.get('/', async (req, res) => {
+app.get('/', checkAuth, async (req, res) => {
     const retrievedGames = await retrieveGamesByGenre('Rol');
-    res.render('index', { title: 'Home', firstRow: retrievedGames.firstRow, secondRow: retrievedGames.secondRow });
+    res.render('index', { title: 'Home', firstRow: retrievedGames.firstRow, secondRow: retrievedGames.secondRow , isLoggedIn: req.isLoggedIn});
 });
 app.get('/signUp', (req, res) => {
     res.render('signUp', { title: 'Registrarse' });
@@ -27,7 +29,29 @@ app.get('/game/:gameName', async (req, res) => {
     const gameName = req.params.gameName;    
     res.render('game', { title: gameName });
 })
+app.get('/logout', (req, res) => {
+    res.clearCookie('token');  
+    res.redirect('/'); 
+});
 app.use((req, res) => {
     res.status(404).send('404: Page Not Found');
 });
+
+function checkAuth(req, res, next) {
+    const token = req.cookies.token; // Get token from cookies
+
+    if (!token) {
+        req.isLoggedIn = false;
+        return next();
+    }
+
+    jwt.verify(token, 'videogame_review_web_site', (err, decoded) => {
+        if (err) {
+            req.isLoggedIn = false;
+        } else {
+            req.isLoggedIn = true;
+        }
+        next();
+    });
+}
 export default app;
